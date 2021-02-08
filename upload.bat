c2ed65c607341541a4c21d5adaef9276a8ec6d25
daniel-kanchev <daniel.kanchev@adata.pro>
2021-02-08 14:23:27 +0200

    Update

--- upload.bat
+++
@@ -1,6 +0,0 @@
-git init -b main
-git add .
-git commit -m "Finished Script"
-gh repo create utbank -y --public
-git remote add origin https://github.com/daniel-kanchev/utbank.git
-git push origin main
--- utbank/spiders/ut.py
+++ utbank/spiders/ut.py
@@ -10,30 +10,32 @@ class UtSpider(scrapy.Spider):
     start_urls = ['https://www.utbank.co.uk/news-and-media/']
 
     def parse(self, response):
-        links = response.xpath('').getall()
-        yield from response.follow_all(links, self.parse_article)
+        articles = response.xpath('//article')
+        for article in articles:
+            link = article.xpath('./div[@class="media-and-news-post-inner"]/div[@class="text"]/a/@href').get()
+            date = " ".join(article.xpath('.//time//text()').get().split()[1:])[:-1]
+            yield response.follow(link, self.parse_article, cb_kwargs=dict(date=date))
 
-    def parse_article(self, response):
+        next_page = response.xpath('//a[@class="next page-numbers"]/@href').get()
+        if next_page:
+            yield response.follow(next_page, self.parse)
+
+    def parse_article(self, response, date):
         item = ItemLoader(Article())
         item.default_output_processor = TakeFirst()
 
-        title = response.xpath('').get().strip()
-        date = response.xpath('').get().strip()
-        date = datetime.strptime(date, '')
+        title = response.xpath('//h1[@class="mainheader"]/text()').get().strip()
+
+        date = datetime.strptime(date, '%d %B %Y')
         date = date.strftime('%Y/%m/%d')
-        content = response.xpath('').getall()
+
+        content = response.xpath('//div[@class="two-thirds"]//text()').getall()
         content = [text for text in content if text.strip()]
-        content = "\n".join(content).strip()
+        content = "\n".join(content[1:]).strip()
 
         item.add_value('title', title)
         item.add_value('date', date)
         item.add_value('link', response.url)
         item.add_value('content', content)
-        # item.add_value('author', author)
-        # item.add_value('category', category)
-        # item.add_value('tags', tags)
 
         return item.load_item()
-
-# response.xpath('').get()
-
